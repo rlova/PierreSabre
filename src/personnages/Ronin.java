caaaba7b76095d994fafe2cbf941eb8250155aae
rlova <lovaremano@gmail.com>
2024-10-28 21:50:17 +0100

    ajout classe Ronin

--- src/personnages/Ronin.java
+++ src/personnages/Ronin.java
@@ -1,5 +1,14 @@
 package personnages;
 
-public class Ronin {
-
+public class Ronin extends Humain {
+	private int honneur = 1;
+	
+	public Ronin(String nom, String boisson, int argent) {
+		super(nom, boisson, argent);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void donner(Commercant beneficiaire) {
+		return beneficiaire.getArgent()*(1-(10/100));
+	}
 }
